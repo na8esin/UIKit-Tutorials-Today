e464b5d9ee86074a9f6ebd0516ea8ddc981f26fb
Takayuki Watanabe <watanabe@sikmi.com>
2022-09-25 10:23:32 +0900

    create struct Reminder

---
+++ Today/Models/Reminder.swift
@@ -0,0 +1,15 @@
+//
+//  Reminder.swift
+//  Today
+//
+//  Created by 渡辺孝之 on 2022/09/23.
+//
+
+import Foundation
+
+struct Reminder {
+    var title: String
+    var dueDate: Date
+    var notes: String? = nil
+    var isComplete: Bool = false
+}
